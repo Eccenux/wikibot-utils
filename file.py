b4b478ed07a77eb78cd134fd8d812fed0d9dcffc
Nux <eccenux+git@gmail.com>
2022-05-03 01:04:29 +0200

    download pages

---
+++ file.py
@@ -0,0 +1,21 @@
+import os, re
+# pip install Unidecode
+from unidecode import unidecode
+
+def save_page_content(page, base_path):
+	"""
+	Save page contents to a file.
+	
+	@param page pywikibot page object.
+	@param output_path Base directory.
+	"""
+	# safe name for a file
+	titleTrans = page.title()
+	titleTrans = unidecode(titleTrans)
+	titleTrans = re.sub(r'[:/]', r'.', titleTrans)
+	titleTrans = re.sub(r'[^a-zA-Z0-9.]', r'_', titleTrans)
+	file = "page_" + titleTrans + ".id=" + str(page.pageid) + ".txt"
+	#print("\t", file)
+	path = os.path.join(base_path, file)
+	with open(path, "w+", encoding='utf-8') as text_file:
+		text_file.write(page.text)
