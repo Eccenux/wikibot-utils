2f8a00228c46c8488e30a3e2898be2d2dc7ed13b
Nux <eccenux+git@gmail.com>
2022-10-01 20:02:57 +0200

    Save a list (array) to a file.

--- file.py
+++ file.py
@@ -2,6 +2,16 @@ import os, re
 # pip install Unidecode
 from unidecode import unidecode
 
+def make_safe_filename(title):
+	"""
+	Safe name for a file.
+	"""
+	titleTrans = title
+	titleTrans = unidecode(titleTrans)
+	titleTrans = re.sub(r'[:/]', r'.', titleTrans)
+	titleTrans = re.sub(r'[^a-zA-Z0-9.]', r'_', titleTrans)
+	return titleTrans
+
 def save_page_content(page, base_path, suffix = ""):
 	"""
 	Save page contents to a file.
@@ -10,10 +20,7 @@ def save_page_content(page, base_path, suffix = ""):
 	@param output_path Base directory.
 	"""
 	# safe name for a file
-	titleTrans = page.title()
-	titleTrans = unidecode(titleTrans)
-	titleTrans = re.sub(r'[:/]', r'.', titleTrans)
-	titleTrans = re.sub(r'[^a-zA-Z0-9.]', r'_', titleTrans)
+	titleTrans = make_safe_filename(page.title())
 	# must be limited due to limited size of paths on Windows (260 characters by default)
 	titleLimit = 120
 	file = "page_" + titleTrans[:titleLimit] + ".id=" + str(page.pageid) + suffix + ".txt"
@@ -21,3 +28,19 @@ def save_page_content(page, base_path, suffix = ""):
 	path = os.path.join(base_path, file)
 	with open(path, "w+", encoding='utf-8') as text_file:
 		text_file.write(page.text)
+
+def save_list(list_data, base_path, list_name, append = True):
+	"""
+	Save a list (array) to a file.
+	
+	@param page pywikibot page object.
+	@param output_path Base directory.
+	"""
+	file = make_safe_filename(list_name)
+	#print("\t", file)
+	path = os.path.join(base_path, file)
+	access_mode = "w+"
+	if append:
+		access_mode = "a+"
+	with open(path, access_mode, encoding='utf-8') as text_file:
+		text_file.write(str(list_data))
