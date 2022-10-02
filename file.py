b2bab8f99cf139767a9c591cccdf2c6f854f3ba3
Nux <eccenux+git@gmail.com>
2022-10-02 11:35:29 +0200

    save_list_var function and refactor

--- file.py
+++ file.py
@@ -12,7 +12,7 @@ def make_safe_filename(title):
 	titleTrans = re.sub(r'[^a-zA-Z0-9.]', r'_', titleTrans)
 	return titleTrans
 
-def save_page_content(page, base_path, suffix = ""):
+def save_page_content(page, output_path, suffix = ""):
 	"""
 	Save page contents to a file.
 	
@@ -25,20 +25,21 @@ def save_page_content(page, base_path, suffix = ""):
 	titleLimit = 120
 	file = "page_" + titleTrans[:titleLimit] + ".id=" + str(page.pageid) + suffix + ".txt"
 	#print("\t", file)
-	path = os.path.join(base_path, file)
+	path = os.path.join(output_path, file)
 	with open(path, "w+", encoding='utf-8') as text_file:
 		text_file.write(page.text)
 
-def save_list(list_data, base_path, list_name, append = True):
+def save_list_data(list_data:list, output_path, list_name, append = True):
 	"""
 	Save a list (array) to a file.
 	
-	@param page pywikibot page object.
+	@param list_data list of strings.
 	@param output_path Base directory.
+	@param list_name File name.
 	"""
 	file = make_safe_filename(list_name)
 	#print("\t", file)
-	path = os.path.join(base_path, file)
+	path = os.path.join(output_path, file)
 	access_mode = "w+"
 	if append:
 		access_mode = "a+"
@@ -47,3 +48,21 @@ def save_list(list_data, base_path, list_name, append = True):
 		if size > 0:
 			text_file.write(",\n")
 		text_file.write(str(list_data))
+
+def save_list_var(output_path, list_name, var_name = 'pages'):
+	"""
+	Use after `save_list_data` to make a valid Python file (with a list of lists).
+	
+	@param output_path Base directory.
+	@param list_name File name.
+	"""
+	file = make_safe_filename(list_name)
+	path = os.path.join(output_path, file)
+	with open(path, "r+", encoding='utf-8') as text_file:
+		text_file.seek(0)
+		text = text_file.read()
+		text_file.seek(0)
+		text_file.write(f"{var_name} = [\n")
+		text_file.write(text)
+		text_file.write("\n]\n")
+
